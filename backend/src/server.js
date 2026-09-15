80230a48e5fedcd0f9970eeb64ab5392b918daee
zaynahanjum <zaynahanjum789@gmail.com>
2026-09-15 19:49:47 +0530

    fixed build error in vercel.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,18 @@ const connectDB = require('./config/db');
 // Connect to Database
 connectDB();
 
-// Start Server
-const server = app.listen(config.port, () => {
-  console.log(`Server running in ${config.env} mode on port ${config.port}`);
-});
+if (!process.env.VERCEL) {
+  // Start Server
+  const server = app.listen(config.port, () => {
+    console.log(`Server running in ${config.env} mode on port ${config.port}`);
+  });
 
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.error(`Error: ${err.message}`);
-  // Close server & exit process
-  server.close(() => process.exit(1));
-});
+  // Handle unhandled promise rejections
+  process.on('unhandledRejection', (err, promise) => {
+    console.error(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+  });
+}
+
+module.exports = app;
